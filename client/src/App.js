0e4e34b1c441e8c20fec0dc73be68112122d8e3c
Boaz <boaserem022@gmail.com>
2022-09-23 17:11:56 +0300

    data fetch using axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,46 @@
 import React from "react";
+import axios from "axios";
 
 
 export const App = ()=> {
-  return (
-    <div >
-      This is the client side
 
-      <form action="../../post" method="post" 
-              className="form">
-          <button type="submit">Connected?</button>
-        </form>
-    </div>
-  );
+  const [data, setData ] = React.useState("");
+
+  const url = "http://localhost:8080";
+
+  React.useEffect(() => {
+    getData();
+
+  }, []);
+
+  const getData =()=>{
+    axios.get(`${url}data`)
+    .then((response) => {
+      const allData = response.data;
+
+      setData(allData);
+
+    })
+
+    .catch((error) => {console.error(`Error: ${error}`)});
+  }
+
+  
+  if(data.length > 0 ) {
+
+    
+      data.map((data)=>{
+        return (
+          <div >
+            This is the client side
+            <h1>{data.name}</h1>
+        
+          </div>
+        );
+      })
+      
+    
+  }
 }
 
 
