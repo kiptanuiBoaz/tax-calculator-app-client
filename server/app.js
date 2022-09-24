9b7a1b0de0655d1ad15979f45db71244fcf0e6c1
owen254 <owenmiash90@gmail.com>
2022-09-24 16:26:20 +0300

    Individual Tax Bands and Rates functional

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const cors=require('cors')
+
+app.use(cors())
 app.use(express.json())
 const paye=require('./routes/paye')
 app.use('/api',paye);
--- server/routes/paye.js
+++ server/routes/paye.js
@@ -2,52 +2,72 @@ const router=require('express').Router()
 
 router.post('/payeCalculator',(req,res)=>{
         //   let contribution=
-        let {grossSalary,paymentPeriod,contributionBenefit,mortageInterest,insuranceRelief,homeOwnership}=req.body;
-        if(!grossSalary) return res.status(404).json("Please set your gross salary")
+        let {grossSalary,paymentPeriod,contributionBenefit,mortageInterest,insuranceRelief,homeOwnership,disability}=req.body;
+        if(!grossSalary) return res.status(404).json("Please set your gross salary");
         contributionBenefit=contributionBenefit ? contributionBenefit : 1080;
-        personalRelief=2400
-        totalReductions=contributionBenefit + mortageInterest;
-        taxableAmount=grossSalary - totalReductions
-        taxPayable=0
-        return res.status(200).json({taxableAmount})
-//         if(taxableAmount > 24000){
-//            remainingAmount=taxableAmount - 24000
-//            taxDue1=24000 * 0.1;
-//            taxPayable += taxDue1
-          
+        insuranceRelief=insuranceRelief ? insuranceRelief * 0.15 : 0;
+        // Declaraing default values
+        personalRelief=2400;
+        PAYE=0;
+        totalTax=0;
+        totalReductions=disability ? grossSalary + contributionBenefit + mortageInterest : contributionBenefit + mortageInterest;
+        taxableIncome=grossSalary - totalReductions;
 
-//                 if(remainingAmount > 8333){
-//                     remainingAmount -=8333;
-//                     taxDue2=8333 * 0.25
-//                     taxPayable +=taxDue2
-                   
-                           
 
-//                     if(remainingAmount > 32332){
-//                         finalTaxDue=remainingAmount * 0.3
-//                         taxPayable += finalTaxDue
-                       
-//                     }
-//                     }
-               
-//                        else{
-//                          PAYE=taxPayable - 2400;
-//                         return res.status(200).json({taxPayable,PAYE})
-//                        }
-//         }else {
-//             return PAYE=0
-//         }
-        
-// // const taxRates=[
-// //     {level:"first",amount:24000,applicableRates:0.1},
-// //     {level:"second",amount:8333,applicableRates:0.25},
-// //     {level:"final",amount:32332,applicableRates:0.3},
-// // ]
-// // const taxPayable=taxRates.reduce((currentTotal,item)=>{
-// //  return item.amount
-// // },0)
+        if(taxableIncome > 24000){
+           remainingAmount=taxableIncome - 24000;
+           taxDue1=24000 * 0.1;
+           totalTax += taxDue1;
+
+           if(remainingAmount >8333){
+                remainingAmount-=8333;
+                taxDue2=8333 * 0.25;
+                totalTax += taxDue2;
+           }
+           else{
+                taxDue2=remainingAmount * 0.25;
+                totalTax += taxDue2;
+                return res.status(200).json({finalTaxDue,remainingAmount,totalTax});
+           }
+           if(remainingAmount > 32332){
+                finalTaxDue=remainingAmount * 0.3;
+                totalTax +=finalTaxDue;
+            }
+           PAYE=totalTax - personalRelief - insuranceRelief;
+           netPay=taxableIncome - PAYE;
+           return res.status(200).json({grossSalary,netPay,contributionBenefit,taxableIncome,totalTax,personalRelief,insuranceRelief,PAYE});
+
+        }
+        else  {
+              netPay = disability ? totalReductions : taxableIncome;  
+        return res.status(200).json({grossSalary,netPay,contributionBenefit,taxableIncome,totalTax,personalRelief,insuranceRelief,PAYE});
+        }
+
+
+
+
 })
 
 
 
 module.exports=router
+// if(remainingAmount > 8333){
+        //                     remainingAmount -=8333;
+        //                     taxDue2=8333 * 0.25
+        //                     taxPayable +=taxDue2
+                           
+                                   
+        
+        //                     if(remainingAmount > 32332){
+        //                         finalTaxDue=remainingAmount * 0.3
+        //                         taxPayable += finalTaxDue
+                               
+        //                     }
+        //                     }
+                       
+        //                        else{
+        //                          PAYE=taxPayable - 2400;
+        //                         return res.status(200).json({taxPayable,PAYE})
+        //                        }
+        //         }else {
+        //             return PAYE=0
