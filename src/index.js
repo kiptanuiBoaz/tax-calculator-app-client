5ecd8dd20b92439ee06b04d09865d283b6a69b75
kiptanui boaz <boaserem022@gmail.com>
2023-01-12 11:44:42 +0300

    merge

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {App} from './App';
 import {store} from "./app/store";
 import { Provider } from "react-redux";
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
